322bdee0719a98996a0a5add9a39ac99b95280bb
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-08-11 02:22:52 -0300

    adiciona mock para solicitação de relatorio

---
+++ ExemploRabbitMQ.Api/Data/Relatorio/SolicitacaoRelatorio.cs
@@ -0,0 +1,38 @@
+﻿namespace ExemploRabbitMQ.Api.Data.Relatorio;
+
+public static class RelatorioData
+{
+    public static List<SolicitacaoRelatorio> Relatorios { get; } = new List<SolicitacaoRelatorio>
+    {
+        new SolicitacaoRelatorio
+        {
+            Id = Guid.NewGuid(),
+            Nome = "Relatório de Vendas",
+            Status = Status.Pendente,
+            DataProcessamento = null
+        },
+        new SolicitacaoRelatorio
+        {
+            Id = Guid.NewGuid(),
+            Nome = "Relatório de Estoque",
+            Status = Status.EmProcessamento,
+            DataProcessamento = null
+        }
+    };
+}
+
+public class SolicitacaoRelatorio
+{
+    public Guid Id { get; set; }
+    public string? Nome { get; set; }
+    public Status Status { get; set; }
+    public DateTime? DataProcessamento { get; set; }
+}
+
+public enum Status
+{
+    Pendente = 0,
+    EmProcessamento = 1,
+    Concluido = 2,
+    Erro = 3
+}
