af969d7562211fae9ea62ee56db551229e4f423d
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-08-11 02:24:09 -0300

    configura vriaveis de ambiente

---
+++ ExemploRabbitMQ.Api/Extensions/AppExtensions.cs
@@ -0,0 +1,29 @@
+﻿using MassTransit;
+
+namespace ExemploRabbitMQ.Api.Extensions;
+
+public static class AppExtensions
+{
+    public static void AddRabbitMQService(this IServiceCollection services, IConfiguration config)
+    {
+        var host = Environment.GetEnvironmentVariable("RABBITMQ_HOST") ?? config["RabbitMQHost:Host"] ?? "my-rabbit";
+        var port = Environment.GetEnvironmentVariable("RABBITMQ_PORT") ?? config["RabbitMQHost:Port"] ?? "5672";
+        var username = Environment.GetEnvironmentVariable("RABBITMQ_USERNAME") ?? config["RabbitMQHost:Username"] ?? "guest";
+        var password = Environment.GetEnvironmentVariable("RABBITMQ_PASSWORD") ?? config["RabbitMQHost:Password"] ?? "guest";
+
+        services.AddMassTransit(x =>
+        {
+            x.UsingRabbitMq((context, cfg) =>
+            {
+                cfg.Host(new Uri($"amqp://{host}:{port}"), h =>
+                {
+                    h.Username(username);
+                    h.Password(password);
+                });
+
+                cfg.ConfigureEndpoints(context);
+                cfg.UseMessageRetry(r => r.Exponential(10, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(60), TimeSpan.FromSeconds(5)));
+            });
+        });
+    }
+}
