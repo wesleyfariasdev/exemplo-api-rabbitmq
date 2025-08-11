ca235d5b44c1c1da2c707cffa18ce345d6a80e87
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-08-11 02:24:49 -0300

    AddRabbitMQService

--- ExemploRabbitMQ.Api/Program.cs
+++ ExemploRabbitMQ.Api/Program.cs
@@ -1,8 +1,11 @@
+using ExemploRabbitMQ.Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddRabbitMQService(builder.Configuration);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -13,10 +16,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ExemploRabbitMQ.Api v1"));
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
