6ed70821a58be00375d7cf13570752743ee27a90
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-08-11 02:22:37 -0300

    mock das api

---
+++ ExemploRabbitMQ.Api/Controllers/ApiController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExemploRabbitMQ.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ObterRelatorios() =>
+            Ok(Data.Relatorio.RelatorioData.Relatorios);
+
+        [HttpPost]
+        public IActionResult SolicitarRelatorio([FromBody] string nomeRelatorio)
+        {
+            var novoRelatorio = new Data.Relatorio.SolicitacaoRelatorio
+            {
+                Id = Guid.NewGuid(),
+                Nome = nomeRelatorio,
+                Status = Data.Relatorio.Status.Pendente,
+                DataProcessamento = null
+            };
+            Data.Relatorio.RelatorioData.Relatorios.Add(novoRelatorio);
+            return CreatedAtAction(nameof(ObterRelatorios), new { id = novoRelatorio.Id }, novoRelatorio);
+        }
+    }
+}
